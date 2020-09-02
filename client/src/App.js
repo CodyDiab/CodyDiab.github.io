ddfd16fb9e170b269d74bc38c7b8d9d66c6d2208
Diab <emailcodydiab@gmail.com>
2020-09-02 08:30:05 -0500

    set up react structure

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from './components/header'
+// import './App.css';
+
+function App() {
+
+  return (
+    <Router>
+     <Header/>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ client/src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header() {
+
+    return (
+        <header>
+   <h1> cody nabil diab</h1>
+      front-end developer 
+
+    </header>
+    )
+}
+
+export default Header;
