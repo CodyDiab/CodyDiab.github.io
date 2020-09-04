b7e23a1fe9f3abde81a1ad953d2dc07501b17374
Diab <emailcodydiab@gmail.com>
2020-09-04 10:17:27 -0500

    work styles

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -14,8 +14,9 @@ function Contact() {
              </figure>
            <div className="content">
             <p> 
-            <a href="tel:254-749-3041">254-749-3041</a>
-            <a href="mailto:emailcodydiab@gmail.com@example.com">emailcodydiab@gmail.com</a>
+            <a href="tel:254-749-3041">254-749-3041</a><br></br>
+            <a href="mailto:emailcodydiab@gmail.com@example.com">emailcodydiab@gmail.com</a> <br></br>
+            <a href="https://github.com/CodyDiab" target="_blank">https://github.com/CodyDiab</a>
             </p>
            </div>
         </div>
--- client/src/components/work/index.js
+++ client/src/components/work/index.js
@@ -13,17 +13,61 @@ function Work() {
         totalSlides={6}
       >
           <Slider className="card-image">
-              <Slide className="gallery-image"index={0}><img alt="screenshot" src={require("../../assets/scatterbrain.png")}></img></Slide>
-              <Slide className="gallery-image" index={1}><img alt="screenshot" src={require("../../assets/IRISscreenshot.png")}></img></Slide>
-              <Slide className="gallery-image" index={2}><img alt="screenshot" src={require("../../assets/paper-locator.png")}></img></Slide>
-              <Slide className="gallery-image" index={3}><img alt="screenshot" src={require("../../assets/runbuddy.png")}></img></Slide>
-              <Slide className="gallery-image" index={4}><img alt="screenshot" src={require("../../assets/git-it-done.png")}></img></Slide>
-              <Slide className="gallery-image" index={5}><img alt="screenshot" src={require("../../assets/acess-boost.png")}></img></Slide>
+              <Slide className="gallery-image work-1"index={0}><div className="work-description">
+                  <h1>Pen Name</h1>
+                  <p>A MERN stack social writing platform</p>
+                  <p>
+                  <a target="_blank" href="">Go to live application</a><br></br>
+                  <a target="_blank" href="">Go to GitHub repo</a>
+                  </p>
+                  </div></Slide>
+              <Slide className="gallery-image work-2" index={1}><div className="work-description">
+                  <h1>ScatterBrain</h1>
+                  <p>In browser web resource application</p>
+                  <p>
+                  <a target="_blank" href="">Go to live application</a><br></br>
+                  <a  target="_blank" href="">Go to GitHub repo</a>
+                  </p>
+                  </div></Slide>
+              <Slide className="gallery-image work-3" index={2}><div className="work-description">
+                  <h1>Paper Locator</h1>
+                  <p>Web application for the location of paper products</p>
+                  <p>
+                  <a target="_blank" href="">Go to live application</a><br></br>
+                  <a target="_blank" href="">Go to GitHub repo</a>
+                  </p>
+                  </div></Slide>
+              <Slide className="gallery-image work-4" index={3}><div className="work-description">
+                  <h1>IRIS</h1>
+                  <p>Global weather forcast </p>
+                  <p>
+                  <a target="_blank" href="">Go to live application</a><br></br>
+                  <a  target="_blank" href="">Go to GitHub repo</a>
+                  </p>
+                  </div></Slide>
+              <Slide className="gallery-image work-5" index={4}><div className="work-description">
+                  <h1>Git it Done</h1>
+                  <p>Open source repository search application</p>
+                  <p>
+                  <a target="_blank" href="">Go to live application</a><br></br>
+                  <a target="_blank"  href="">Go to GitHub repo</a>
+                  </p>
+                  </div></Slide>
+              <Slide className="gallery-image work-6" index={5}><div className="work-description">
+                  <h1>Run Buddy</h1>
+                  <p>Optimized UI implimentation</p>
+                  <p>
+                  <a target="_blank"  href="">Go to live application</a><br></br>
+                  <a target="_blank"  href="">Go to GitHub repo</a>
+                  </p>
+                  </div></Slide>
               
           </Slider>
-          <div className="card-content">
-          <ButtonBack className="content">Back</ButtonBack>
-        <ButtonNext className="content">Next</ButtonNext>
+          <div className="card-footer">
+          <div className="card-footer-item">
+          <ButtonBack className="button">Back</ButtonBack>
+        <ButtonNext className="button">Next</ButtonNext>
+        </div>
         </div>
       </CarouselProvider>
       </div>
